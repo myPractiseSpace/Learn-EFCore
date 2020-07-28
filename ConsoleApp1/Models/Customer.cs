ab0928897c4f40b4f711b1cda38660445bb21f2d
tim811223 <nahaf25@gmail.com>
2020-07-28 23:56:26 +0800

    建立Model

---
+++ ConsoleApp1/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Address { get; set; }      //This allows null
+        public string? Phone { get; set; }
+        public string? Email { get; set; }    //This property was added later
+        public ICollection<Order> Orders { get; set; }  //Navigation Property  i.e help internal link to the other class
+    }
+}
