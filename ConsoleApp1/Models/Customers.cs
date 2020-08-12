d8184229996836bd6c1a574903b83552e02b09bc
tim811223 <nahaf25@gmail.com>
2020-08-13 00:12:18 +0800

    DB First 連線DB建立model Scaffold-DbContext "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EFCore;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context CCIPContext -DataAnnotations -Force

---
+++ ConsoleApp1/Models/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleApp1.Models
+{
+    public partial class Customers
+    {
+        public Customers()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        [InverseProperty("Customer")]
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
