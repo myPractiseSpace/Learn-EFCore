2b27d66260bbc6019091c1fb787fe8d89aa4f54b
tim811223 <nahaf25@gmail.com>
2020-08-13 00:21:12 +0800

    建立自訂屬性class  , 就算執行 Scaffold-DbContext語法設定玩的屬性也不會被覆蓋

---
+++ ConsoleApp1/Models/Partial/Products.partial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleApp1.Models
+{
+    [MetadataType(typeof(ProductsMD))]
+    public partial class Products
+    {
+        public class ProductsMD
+        {
+            [Display(Name = "ProdID")]
+            public int Id { get; set; }
+            [Display(Name = "品名")]
+            public string Name { get; set; }
+            [Display(Name = "價格")]
+            public decimal Price { get; set; }
+        }
+    }
+}
