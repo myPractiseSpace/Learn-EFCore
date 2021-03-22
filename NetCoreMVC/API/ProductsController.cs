ff882a793398f9120a07f34bd7bc9baef86e61be
tim811223 <nahaf25@gmail.com>
2021-03-22 13:06:08 +0800

    加入API跟Ajax

---
+++ NetCoreMVC/API/ProductsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCoreMVC.Models.EFCore;
+
+namespace NetCoreMVC.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly EFCoreContext _context;
+
+        public ProductsController(EFCoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public ActionResult<IEnumerable<Products>> GetProducts1()
+        {
+            return _context.Products.ToList();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public ActionResult<Products> GetProducts(int id)
+        {
+            var products = _context.Products.Find(id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+        }
+
+        //// POST: api/Products
+        [HttpPost]
+        public ActionResult UpdateProducts()
+        {
+            try {
+                var ID = Convert.ToInt32(Request.Form["ID"].ToString());
+                var Price = Convert.ToInt32(Request.Form["Price"].ToString());
+                var products = _context.Products.Find(ID);
+                products.Price = Price;
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (DbUpdateConcurrencyException) {
+                //if (!ProductsExists(id)) {
+                //    return NotFound();
+                //}
+                //else {
+                //    throw;
+                //}
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ NetCoreMVC/wwwroot/js/AjaxSetting.js
@@ -0,0 +1,61 @@
+﻿
+function AjaxPost(IsAPI = false, Controller = "", data) {
+    var res = "";
+    var url = '';
+    if (data == undefined || data == null)
+        data = {};
+
+    url = window.location.protocol + "//" + window.location.host;
+    if (IsAPI) {
+        url += ("/api/" + Controller);
+    } else {
+        url += (window.location.pathname + (/[/]$/.test(window.location.pathname)?"":"/") + Controller);
+    }
+
+    $.ajax({
+        async: false,
+        url: url,
+        data: data,
+        type: 'POST',
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status + '/' + xhr.responseText);
+            res = "ERR";
+        },
+        success: function (sdata) {
+            res = "OK" + sdata;
+        }
+    });
+    return res;
+}
+
+
+function AjaxPostFile(ajaxMethod, data) {
+    var res = "";
+    var url = '';
+    if (data == undefined || data == null)
+        data = {};
+
+    url = window.location.protocol + "//" + window.location.host;
+    if (IsAPI) {
+        url += ("/api/" + Controller);
+    } else {
+        url += (window.location.pathname + (/[/]$/.test(window.location.pathname) ? "" : "/") + Controller);
+    }
+
+    $.ajax({
+        async: false,
+        url: url,
+        data: data,
+        type: 'POST',
+        processData: false,
+        contentType: false,
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status + '/' + xhr.responseText);
+            res = "ERR";
+        },
+        success: function (sdata) {
+            res = "OK" + sdata;
+        }
+    });
+    return res;
+}
